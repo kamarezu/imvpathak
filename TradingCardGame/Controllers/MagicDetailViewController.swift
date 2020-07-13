acf7f3905c64954680db60683d477c37bc6b5f31
kamarezu <116710529+kamarezu@users.noreply.github.com>
2020-07-13 08:53:21 +0000

    iptables

---
+++ TradingCardGame/Controllers/MagicDetailViewController.swift
@@ -0,0 +1,13 @@
+
+//
+//  MagicDetailViewController.swift
+//  TradingCardGame
+//
+//  Created by J on 1/9/19.
+//  Copyright © 2019 J. All rights reserved.
+//
+
+import UIKit
+
+class MagicDetailViewController: UIViewController {
+  
