aacc90957feeaaedd59a017504b467672e79740b
kamarezu <116710529+kamarezu@users.noreply.github.com>
2020-09-14 21:19:37 +0000

    Update cookbook.md

--- TradingCardGame/Network Services/MagicIAPIClient .swift
+++ TradingCardGame/Network Services/MagicIAPIClient .swift
@@ -10,4 +10,6 @@ import Foundation
 
 final class magicAPIClient {
     static func MagicInfo(completionHandler: @escaping(AppError?,[magicInfo]?)-> Void) {
-        NetworkHelper.shared.performDataTask(endpointURLString: "https://api.magicthegathering.io/v1/cards?contains=imageUrl", httpMethod: "GET", httpBody: nil) { (appError, data, httpResponse) 
+        NetworkHelper.shared.performDataTask(endpointURLString: "https://api.magicthegathering.io/v1/cards?contains=imageUrl", httpMethod: "GET", httpBody: nil) { (appError, data, httpResponse) in
+            if let appError = appError {
+                completionHandler(appEr
