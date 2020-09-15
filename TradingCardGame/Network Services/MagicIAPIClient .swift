e5cb4214d4b78320f807b1f0483684f147b3c511
kamarezu <116710529+kamarezu@users.noreply.github.com>
2020-09-15 15:25:58 +0000

    Update 1-bundesliga.yml

--- TradingCardGame/Network Services/MagicIAPIClient .swift
+++ TradingCardGame/Network Services/MagicIAPIClient .swift
@@ -12,4 +12,7 @@ final class magicAPIClient {
     static func MagicInfo(completionHandler: @escaping(AppError?,[magicInfo]?)-> Void) {
         NetworkHelper.shared.performDataTask(endpointURLString: "https://api.magicthegathering.io/v1/cards?contains=imageUrl", httpMethod: "GET", httpBody: nil) { (appError, data, httpResponse) in
             if let appError = appError {
-                completionHandler(appEr
+                completionHandler(appError,nil)
+            }
+            guard let response = httpResponse,
+      
